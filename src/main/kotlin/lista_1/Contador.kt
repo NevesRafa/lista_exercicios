a720e16cb0e1ad0da5af23c6fa6a6de6dd6db6f7
Rafael <rafaelneves02@gmail.com>
2022-03-26 21:33:59 -0300

    Feito quinto exercicio da lista 1  (Contador)

---
+++ src/main/kotlin/lista_1/Contador.kt
@@ -0,0 +1,46 @@
+package lista_1
+
+fun main() {
+    contador()
+}
+
+fun contador() {
+
+    var valorInicial = 5
+
+    println("--Para Incrementar [I]")
+    println("--Para Zerar [Z]")
+    println("--Para Retornar o valor [R]")
+    println("--Para Sair [S]")
+    println("---------------------------------------\n")
+
+
+    var opcao: String? = null
+
+    while (opcao != "S") {
+
+        println("Valor do contador = $valorInicial\n")
+
+        println("Digite uma opção:")
+
+        opcao = readLine().toString()
+
+        println("---------------------------------------")
+
+
+        if (opcao == "I") {
+            valorInicial++
+        }
+        if (opcao == "Z") {
+            valorInicial = 0
+        }
+        if (opcao == "R") {
+            valorInicial--
+        }
+
+
+    }
+
+    println("Fim do Programa!!!")
+
+}

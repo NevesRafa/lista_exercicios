c634772e101bdbfa505d72663c471eb095f5fbbb
Rafael <rafaelneves02@gmail.com>
2022-04-06 13:09:02 -0300

    Feito quinto exercicio da lista 2  (Contador De Letras) - correção

--- src/main/kotlin/lista_2/ContadorDeLetras.kt
+++ src/main/kotlin/lista_2/ContadorDeLetras.kt
@@ -8,9 +8,14 @@ class ContadorDeLetras {
         println("Digite um texto ou uma palavra que eu vou contar quantas letras contem para você!!!")
         val texto = readLine().toString()
         val listaPalavras = texto.split(" ")
+        var contador = 0
 
         listaPalavras.forEach { it ->
-            println(it.length)
+            contador += it.length
         }
+
+        println("Contem $contador letras!")
+
+
     }
 }
